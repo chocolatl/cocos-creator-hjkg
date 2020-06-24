fee077f62755123e00d60dc71490711efeeba5f1
chocolatl <chocolatl.moe@outlook.com>
2020-06-25 01:29:07 +0800

    deploy

---
+++ src/settings.864cb.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-desktop",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:false,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/main.fire",orientation:"",jsList:[],bundleVers:{internal:"b25de",main:"a06ca"}};
